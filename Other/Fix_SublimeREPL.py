198e957049edb70b7414b798d96b35e16c27fb8d
NoobAndPoor <napthedevhcj@gmail.com>
2021-06-28 20:28:26 +0700

    change to fix_sublimeREPL

--- Other/Fix_SublimeREPL.py
+++ Other/Fix_SublimeREPL.py
@@ -1,7 +1,14 @@
 import os
-import getpass
+try:
+    import getpass
+except Exception:
+    os.system("pip install getpass")
 
 USER = getpass.getuser()
 PATH = r"C:\Users\{}\AppData\Roaming\Sublime Text\Packages\User\.SublimeREPLHistory\python.db".format(USER)
 
-os.remove(PATH)
+if os.path.exists(PATH):
+    os.remove(PATH)
+    print("Sublime REPL fixed")
+else:
+    print("Sublime REPL has already been fixed")
