c96d6537d58b66adf0206b395cd1938d647c5365
NoobAndPoor <napthedevhcj@gmail.com>
2021-07-17 19:30:06 +0700

    update setup file

--- Mediapipe/background_removal.py
+++ Mediapipe/background_removal.py
@@ -6,7 +6,6 @@ mp_selfie_segmentation = mp.solutions.selfie_segmentation
 selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation()
 
 capture = cv2.VideoCapture(0)
-bg_img = None
 while True:
     success, img = capture.read()
     if not success:
@@ -15,15 +14,14 @@ while True:
     RGB_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = selfie_segmentation.process(RGB_image)
 
-    condition = np.stack(
-        (results.segmentation_mask,) * 3, axis=-1) > 0.1
+    condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
 
-    if bg_img is None:
-        bg_img = np.zeros(img.shape, dtype=np.uint8)
-        bg_img[:] = (192, 192, 192)
+    bg_img = cv2.GaussianBlur(img, (99, 99), 0)
+
     output_img = np.where(condition, img, bg_img)
 
     cv2.imshow("Camera", output_img)
     if cv2.waitKey(1) & 0xFF == 27:
         break
+
 capture.release()
